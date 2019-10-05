2f395eef1cc43eae23a6c70bdaad980c0a29eb4b
Mohammed Zayan <zayanit@gmail.com>
2019-10-05 18:49:10 +0300

    Add breadcrumb English translation

---
+++ resources/lang/en/breadcrumb.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | breadcrumb Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | breadcrumb translation.
+    |
+    */
+
+    'dashboard'  => 'Dashboard',
+    'banners'    => 'Banners',
+    'image'      => 'Image',
+    'add_banner' => 'Add banner',
+
+];
