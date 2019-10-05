5a1e83b094c07b0d20f3b67d3c9818fc7489a486
Mohammed Zayan <zayanit@gmail.com>
2019-10-05 19:01:01 +0300

    Add passwords Arabic translation

---
+++ resources/lang/ar/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'يجب أن تتكون كلمات المرور من ستة أحرف على الأقل وأن تطابق التأكيد.',
+    'reset'    => 'تم إعادة تعيين كلمة المرور الخاصة بك!',
+    'sent'     => 'لقد أرسلنا رابط إعادة تعيين كلمة المرور عبر البريد الإلكتروني!',
+    'token'    => 'رمز إعادة تعيين كلمة المرور غير صالح.',
+    'user'     => "لا يمكننا العثور على مستخدم يحمل عنوان البريد الإلكتروني هذا.",
+
+];
