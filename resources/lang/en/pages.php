0f60cf950df127411e49181a48f7c3ff560532c4
Mohammed Zayan <zayanit@gmail.com>
2019-10-05 18:57:58 +0300

    Add pages English translation

---
+++ resources/lang/en/pages.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pages Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Pages translation.
+    |
+    */
+    
+    'dashboard'            => 'Dashboard',
+    'pages'                => 'Pages',
+    'list_pages'           => 'Pages list',
+    'create_page'          => 'Create page',
+    'code'                 => 'Code',
+    'title'                => 'Title',
+    'body'                 => 'Body',
+    'link'                 => 'Link',
+    'provide_menu_link'    => 'Add the page in the list',
+    'menus'                => 'Menus',
+    'menu_select'          => 'Select the menu',
+    'created_at'           => 'Created at',
+    'created_by'           => 'Author',
+    'last_update'          => 'Last update',
+    'operations'           => 'Operations',
+    'edit'                 => 'Edit',
+    'delete'               => 'Delete',
+    'no_results'           => 'No results',
+    'page'                 => 'Page',
+    'total'                => 'Total',
+    'add_banner'           => 'Add banner',
+    'max_size'             => 'Max size',
+    'supported_extantions' => 'Supported extantions',
+    'save'                 => 'Save',
+    'update'               => 'Update',
+    'edit_page'            => 'Edit page',
+    'saved_successfully'   => 'Saved successfully',
+    'updated_successfully' => 'Updated successfully',
+    'deleted_successfully' => 'Deleted successfully',
+    'image'                => 'Banner',
+    'page_delete_confirm'  => 'Confirm page deletion',
+    'delete_confirm_msg'   => 'Are you sure you want to delete?',
+    'cancel'               => 'Cancel',
+
+];
